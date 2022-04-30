c112fbd63fdff172e630bba6b9cd48501747669c
Guilherme Jacintho <guilhermejacintho@gmail.com>
2022-04-30 16:07:17 -0300

    Adding of application setting to store the app last execution

---
+++ NobelPrizeApp/Model/ApplicationSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NobelPrizeApp.Model
+{
+    public class ApplicationSettings
+    {
+        public string LastExecution { get; set; }
+    }
+}
