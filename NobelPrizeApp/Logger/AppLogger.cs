978809bd77c65417f3d15ed17812acf54c6c2876
Guilherme Jacintho <guilhermejacintho@gmail.com>
2022-04-30 16:06:56 -0300

    Update of logging configuration Split the console and file datas Add an error log file

--- NobelPrizeApp/Logger/AppLogger.cs
+++ NobelPrizeApp/Logger/AppLogger.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace NobelPrizeApp.AppLogger
+namespace NobelPrizeApp.ApplicationLogger
 {
     public static class AppLogger
     {
@@ -14,8 +14,23 @@ namespace NobelPrizeApp.AppLogger
 
         public static void LogPrizeInformation(NobelPrizeInformation prizeInformation)
         {
-            Logger.Error(JsonConvert.SerializeObject(prizeInformation.prizes));
+            Logger.Info(String.Concat("API Answer: ", JsonConvert.SerializeObject(prizeInformation.prizes)));
         }
+
+        public static void LogPrizeRequest(RequestInformation prizeRequest)
+        {
+            Logger.Trace($"Requesting Nobel Prizes for {prizeRequest.category} on the year {prizeRequest.year}.");
+            Logger.Info($"Requesting Nobel Prizes for {prizeRequest.category} on the year {prizeRequest.year}.");
+            Logger.Trace($"URL: https://api.nobelprize.org/v1/prize.json?category={prizeRequest.category}&year={prizeRequest.year}");
+        }
+
+        public static void LogPrizeError(string errorMessage)
+        {
+            Logger.Info("An error has ocurred on retrieving prize information.");
+            Logger.Error(errorMessage);
+        }
+
+
 
     }
 }
