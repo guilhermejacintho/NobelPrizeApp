ce3dd27ecaf54114c282788664e32c3af11a6537
Guilherme Jacintho <guilhermejacintho@gmail.com>
2022-04-30 14:13:18 -0300

    Adding Log Configuration

---
+++ NobelPrizeApp/Logger/AppLogger.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using NobelPrizeApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NobelPrizeApp.AppLogger
+{
+    public static class AppLogger
+    {
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void LogPrizeInformation(NobelPrizeInformation prizeInformation)
+        {
+            Logger.Error(JsonConvert.SerializeObject(prizeInformation.prizes));
+        }
+
+    }
+}
