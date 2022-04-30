6baa77485680399b86df9d928ffe29eccecb3827
Guilherme Jacintho <guilhermejacintho@gmail.com>
2022-04-30 10:03:13 -0300

    Add of model classes

---
+++ NobelPrizeApp/FileReader/IFileReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NobelPrizeApp.FileReader
+{
+    interface IFileReader
+    {
+        string[] ReadFile(string path);
+    }
+}
