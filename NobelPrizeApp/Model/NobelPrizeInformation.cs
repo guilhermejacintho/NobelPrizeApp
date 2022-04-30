cdbe42188d250b8407c85282347867a615dcd93d
Guilherme Jacintho <guilhermejacintho@gmail.com>
2022-04-30 10:02:23 -0300

    Model classes definition

---
+++ NobelPrizeApp/Model/NobelPrizeInformation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NobelPrizeApp.Model
+{
+    public class NobelPrizeInformation
+    {
+        public Prize[] prizes;
+    }
+
+    public class Prize
+    {
+        public string year;
+        public string category;
+        public Laureate[] laureates;
+    }
+
+    public class Laureate
+    {
+        public string id;
+        public string firstname;
+        public string surname;
+        public string motivation;
+        public string share;
+    }
+
+}
---
+++ NobelPrizeApp/Model/NobelPrizeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NobelPrizeApp.Model
+{
+    public class NobelPrizeRequest
+    {
+        public RequestInfromation[] requests;
+    }
+
+    public class RequestInfromation
+    {
+        public string category;
+        public string year;
+    }
+}
